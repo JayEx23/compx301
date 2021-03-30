659dde6a141d37715d0ca1c54ea13368f926a9f6
jr137 <jr137@lab-rg06-19.cms.waikato.ac.nz>
2021-03-31 12:20:30 +1300

    Let CreateRuns read all lines from standard input

--- CreateRuns.java
+++ CreateRuns.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 //
 // CreateRuns class
 //
@@ -20,6 +23,26 @@ public class CreateRuns {
             System.out.println(msgUsage);
             return;
         }
-        System.out.println(heapSize);
+        System.out.println(heapSize); // !Debug print statement
+        read();
+    }
+
+    //
+    // Read standard input
+    //
+    public static void read() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            // Read until end of standard input
+            // e.g. `cat test.txt | java CreatRuns 10`
+            // should read all lines from test.txt and then stop
+            String line = new String(reader.readLine());
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
