c95b7ad21a0e29f11bb8226a4d8b6d4886e956fa
Jesse <jesse@DESKTOP-C3UAF90.localdomain>
2021-04-02 16:55:39 +1300

    update constructor to initialize size and heap array variables

--- MyMinHeap.java
+++ MyMinHeap.java
@@ -1,7 +1,15 @@
-public class MyMinHeap{
+public class MyMinHeap {
+
+    private int _size;
+    private String[] _minHeap;
 
     // The constructor for minHeap object
-    public MyMinheap() {
-        
+    public MyMinHeap(int size) {
+        if (size < 1) {
+            System.out.println("Please provide a heap size greater than 1");
+            return;
+        }
+        _size = size;
+        _minHeap = new String[_size];
     }
 }
