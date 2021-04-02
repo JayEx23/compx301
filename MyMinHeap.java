68eaa652592bbb9ebc670c506d56c03ef7479e98
JayEx23 <eugenechew2012@gmail.com>
2021-04-02 22:14:23 +1300

    finished swap working on remove and downHeap

--- MyMinHeap.java
+++ MyMinHeap.java
@@ -6,11 +6,13 @@ public class MyMinHeap {
     // The constructor for minHeap object
     public MyMinHeap(int size) {
         if (size < 1) {
-            System.out.println("Please provide a heap size greater than 1");
-            return;
+            _size = 32;
+            _minHeap = new String[_size];
+        } else {
+            _size = size;
+            _minHeap = new String[_size];
         }
-        _size = size;
-        _minHeap = new String[_size];
+
     }
 
     // Upheap array
@@ -18,24 +20,51 @@ public class MyMinHeap {
         // Iterate from bottom to top
         int i = _size;
         while (i > 1) {
-            int parentIndex = (i-1)/2; // "i-2" because our heap array's first item starts at index 0
+            int parentIndex = (i - 1) / 2; // "i-2" because our heap array's first item starts at index 0
             String current = new String(_minHeap[i]);
             String parent = new String(_minHeap[parentIndex]);
             if (current.compareTo(parent) < 0) { // If current is less than parent
                 // Perform swap
-                _minHeap[parentIndex] = current;
-                _minHeap[i] = parent;
+                // _minHeap[parentIndex] = current;
+                // _minHeap[i] = parent;
+                swap(parentIndex, i);
             } else {
                 return; // Upheap complete
             }
         }
     }
 
+    private void downheap(int index) {
+
+    }
+
     // Insert into heap
     public void insert(String value) {
-        _size ++;
-        _minHeap[_size-1] = value;
+        _size++;
+        _minHeap[_size - 1] = value;
         upheap();
+    }
+
+    // Remove from heap
+    public void remove(String value) {
+        int root = 0;
+        int tail = _size;
+
+        try {
+            if (_size > 0) {
+                swap(root, tail);
+                _size--;
+            }
+        } catch (Exception x) {
+            System.err.println(x);
+        }
+    }
+
+    // Swaps the position of 2 index/items in the array
+    private void swap(int j, int k) {
+        String temp = new String(_minHeap[j]);
+        _minHeap[j] = _minHeap[k];
+        _minHeap[k] = temp;
     }
 
     // Print heap array in raw format
