4c9022d40ba9639559173caf0b27b1ed4118ecba
Jesse <jesse@DESKTOP-C3UAF90.localdomain>
2021-04-07 23:19:41 +1200

    Change min heap interface to be generic, add merge min heap which uses string[] instead of string

--- MyMergeMinHeap.java
+++ MyMergeMinHeap.java
@@ -1,27 +1,28 @@
-public class MyMinHeap implements IMinHeap<String> {
+public class MyMergeMinHeap implements IMinHeap<String[]> {
 
     private int _size;
-    private String[] _minHeap;
+    private String[][] _minHeap;
 
     // The constructor for minHeap object
-    public MyMinHeap(int size) {
+    public MyMergeMinHeap(int size) {
         if (size < 1) { // Use default size
-            _minHeap = new String[32];
+            _minHeap = new String[32][2];
             _size = 0;
         } else { // Use specified value
-            _minHeap = new String[size];
+            _minHeap = new String[size][2];
             _size = 0;
         }
 
     }
 
     // Insert into heap
-    public void insert(String value) {
+    public void insert(String[] value) {
         if (_size == _minHeap.length) {
             System.out.println("Warning: minheap is full -- could not insert value!");
         } else {
             _size++;
-            _minHeap[_size - 1] = value;
+            _minHeap[_size - 1][0] = value[0];
+            _minHeap[_size - 1][1] = value[1];
             upheap();
         }
     }
@@ -42,20 +43,33 @@ public class MyMinHeap implements IMinHeap<String> {
 
     // peek
     public String peek() {
-        return _minHeap[0];
+        return _minHeap[0][0];
+    }
+
+    // peek source
+    public String peekSource() {
+        return _minHeap[0][1];
     }
 
     // load values into heap without regard for heap order
-    // args - values: a string collection to load into our heap
-    public void load(String[] values) {
+    // args - values: a string collection to load into our heap, along with names of sources for
+    // where these values came from
+    public void load(String[][] values) {
+        // Check if input values are in correct format
+        if (values[0].length < 2 && values[0].length > 2) {
+            System.out.println("Warning: merge minheap can only accept format String[x][2]");
+            return;
+        }
         _size = 0;
         // Load values
-        for (int i = 0; i < _minHeap.length; i++) {
-            if (i >= values.length) {
-                break;
+        for (int sourceIndex = 0; sourceIndex < 2; sourceIndex ++) {
+            for (int i = 0; i < _minHeap.length; i++) {
+                if (i >= values.length) {
+                    break;
+                }
+                _minHeap[i][sourceIndex] = values[i][sourceIndex];
+                _size++;
             }
-            _minHeap[i] = values[i];
-            _size++;
         }
         // Warn user if input values could not all fit in minheap
         if (values.length > _minHeap.length) {
@@ -65,9 +79,10 @@ public class MyMinHeap implements IMinHeap<String> {
 
     // Replace root in heap with new value and maintain heap order
     // args - value: the new value
-    public void replace(String value) {
-        _minHeap[0] = value;
-        if (value == null) {
+    public void replace(String[] value) {
+        _minHeap[0][0] = value[0];
+        _minHeap[0][1] = value[1];
+        if (value[0] == null) {
             remove();
         } else { downheap(0); }
     }
@@ -89,24 +104,10 @@ public class MyMinHeap implements IMinHeap<String> {
     // Reset minheap size
     public void reset() {
         for (int i = 0; i < _minHeap.length; i++) {
-            if (_minHeap[i] != null) {
+            if (_minHeap[i][0] != null) {
                 swap(_size, i);
                 _size ++;
             }
-        }
-    }
-
-    // Print heap array in debug format
-    public void print() {
-        System.out.println("Size: " + _size);
-        for (int i = 0; i < _minHeap.length; i++) {
-            if (i == _size) {
-                System.out.println("---size---"); // Visually display where size variable is in effect.
-            }
-            // Print:
-            System.out.print("[" + i + "]\t"); // index
-            System.out.print("[" + (isLeaf(i) ? "L" : "P") + "]\t"); // leaf or parent
-            System.out.println(_minHeap[i]); // item
         }
     }
 
@@ -116,8 +117,8 @@ public class MyMinHeap implements IMinHeap<String> {
         int i = _size - 1;
         while (i > 0) {
             int parentIndex = parentIndex(i);
-            String current = _minHeap[i];
-            String parent = _minHeap[parentIndex];
+            String current = _minHeap[i][0];
+            String parent = _minHeap[parentIndex][0];
             if (current.compareTo(parent) < 0) { // If current is less than parent
                 swap(parentIndex, i);
                 i = parentIndex;
@@ -139,12 +140,12 @@ public class MyMinHeap implements IMinHeap<String> {
             int replaceIndex = leftChildIndex;
 
             if (_size > rightChildIndex(current)) { // We know that there is a child, check right child exists
-                if (_minHeap[rightChildIndex].compareTo(_minHeap[leftChildIndex]) < 0) {
+                if (_minHeap[rightChildIndex][0].compareTo(_minHeap[leftChildIndex][0]) < 0) {
                     replaceIndex = rightChildIndex;
                 }
             }
 
-            if (_minHeap[current].compareTo(_minHeap[replaceIndex]) > 0) {
+            if (_minHeap[current][0].compareTo(_minHeap[replaceIndex][0]) > 0) {
                 swap(current, replaceIndex);
                 current = replaceIndex;
             } else {
@@ -155,9 +156,14 @@ public class MyMinHeap implements IMinHeap<String> {
 
     // Swaps the position of 2 index/items in the array
     private void swap(int j, int k) {
-        String temp = _minHeap[j];
-        _minHeap[j] = _minHeap[k];
-        _minHeap[k] = temp;
+        String tempVal = _minHeap[j][0];
+        String tempSource = _minHeap[j][1];
+
+        _minHeap[j][0] = _minHeap[k][0];
+        _minHeap[j][1] = _minHeap[k][1];
+
+        _minHeap[k][0] = tempVal;
+        _minHeap[k][1] = tempSource;
     }
 
     // Get parent index
@@ -173,14 +179,6 @@ public class MyMinHeap implements IMinHeap<String> {
     // Get right child index
     private int rightChildIndex(int current) {
         return ((current + 1) * 2); // (current+1)*2 because our heap array's first item starts at index 0
-    }
-
-    // checks if the current index is a leaf node (i.e no children)
-    private boolean isLeaf(int current) {
-        if (current > lastParentIndex() && current <= _size) {
-            return true;
-        }
-        return false;
     }
 
     // Get index of last parent item in heap
