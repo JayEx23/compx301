b65c7ead13b8814c5971aa9ffa8dfe823f327c21
Jesse <jesse@DESKTOP-C3UAF90.localdomain>
2021-04-08 00:24:20 +1200

    Add a start to merge() method to merge run files (just need to uncomment the merge method call to test DistributeRuns on it's own :) )

--- MergeRuns.java
+++ MergeRuns.java
@@ -1,17 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+
 public class MergeRuns {
 
     public static void main(String[] args) {
         if(args.length > 1) {
-            System.out.println("Usage");
+            System.out.println("Usage: java MergeRuns <number of files>");
             return;
         }
         try {
-            DistributeRuns dr = new DistributeRuns(Integer.parseInt(args[0]));
+            int numFiles = Integer.parseInt(args[0]);
+            DistributeRuns dr = new DistributeRuns(numFiles);
             dr.createFile();
+
+            String fName = new String(dr.getFilePrefix());
+            merge(numFiles, fName);
         } catch (Exception x) {
             System.out.println(x);
         }
-        
     }
     
+    // Merge temporary run files
+    public static void merge(int numFiles, String fName) throws Exception {
+        BufferedReader[] readers = new BufferedReader[numFiles];
+        // Create file reader for each file
+        for (int fNum = 0; fNum < numFiles; fNum++) {
+            readers[fNum] = new BufferedReader(new FileReader("./" + fName + fNum)); // Assume file is in current directory
+        }
+        // Create output file for each file
+        for (int fNum = 0; fNum < numFiles; fNum++) {
+            new BufferedWriter(new FileWriter("./" + fName + (numFiles + fNum)));
+        }
+        
+        // Create merge minheap
+        MyMergeMinHeap mergeMinHeap = new MyMergeMinHeap(numFiles);
+
+        int currentRunFile = 0;
+        while (true) {
+            // Read lines from files
+            for (int rNum = 0; rNum < readers.length; rNum++) {
+                // Insert line from run and filename into heap
+                mergeMinHeap.insert(new String[] {readers[rNum].readLine(), fName + rNum});
+            }
+
+            // Get current output run file 
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fName + (currentRunFile + numFiles), true));
+
+            // Output smallest value into output run file
+            mergeMinHeap.peek();
+        }
+    }
 }
