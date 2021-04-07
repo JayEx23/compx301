72ff743096655fc70853dc90699cbb46a9589a1e
ehjc1 <ehjc1@lab-rg07-30.cms.waikato.ac.nz>
2021-04-07 12:17:25 +1200

    working on create method

--- DistributeRuns.java
+++ DistributeRuns.java
@@ -1,7 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FilenameFilter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.File;
+import java.io.FileInputStream;
+
 public class DistributeRuns {
+    int _fileNum;
 
+    // Constructor for DistributeRuns
     public DistributeRuns(int fileNum) {
-
+        if(fileNum <= 1) {
+            _fileNum = 2;
+        } else {
+            _fileNum = fileNum;
+        }
     }
 
+    public void createFile() {
+        
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        
+        try{
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt")));
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+
+    }
 }
